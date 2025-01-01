730f4c4b3426f79f70f16b9d00ab3a3e74bd2ef2
Chantha2121 <chanfcsg334@gmail.com>
2025-01-01 16:02:22 +0700

    kotlin variable

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
+val name : String = "chantha"
+var greeting: String? = null
 fun main() {
+    // Test
     println("Hello Kotlin")
+
+    // Variable
+    if(greeting !== null){
+        println(greeting)
+    }
+    else{
+        println("Hi")
+    }
+    println(name)
 }
